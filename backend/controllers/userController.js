1149d1ead93e1d4f3f1e962510f2d505e6fef4e9
jaymbans <88808561+jaymbans@users.noreply.github.com>
2022-06-16 14:35:30 -0500

    authentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -83,7 +83,21 @@ const generateToken = (id) => {
   })
 }
 
+
+//  Get current user
+//  route: /api/users
+//  access: private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name
+  }
+  res.status(200).json(user)
+})
+
 module.exports = {
   registerUser,
-  loginUser
+  loginUser,
+  getMe
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/userController')
+const { registerUser, loginUser, getMe } = require('../controllers/userController')
 
+const { protect } = require('../middleware/authMiddleware'); //when addded as a second argument to the route, it will verify w our protect function
 
 router.post('/', registerUser);
 router.post('/login', loginUser);
+router.get('/me', protect, getMe)
 
 module.exports = router;
