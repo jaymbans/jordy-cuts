88710a82b8c7fa1f89ea53b5ca3d4eb8210577d8
jaymbans <88808561+jaymbans@users.noreply.github.com>
2022-07-05 15:26:00 -0400

    backup port

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 
